f3c6a34eb9e2d630ba77241d747d0db3a4966d0a
Dinesh <dineshsingh9350@gmail.com>
2024-01-25 23:27:52 +0530

    final submit

--- src/components/NavbarCustom.js
+++ src/components/NavbarCustom.js
@@ -68,7 +68,7 @@ const NavbarCustom = () => {
     <Navbar className='nav sticky top-0 bg-transparent  text-white mx-auto max-w-screen-xl px-4 py-2 lg:px-8 lg:py-4 border-none w-full'>
       <div className='container mx-auto flex items-center justify-between text-blue-gray-900'>
         <Link to='/' className='mr-4 cursor-pointer py-1.5 font-bold'>
-          React.js Portfolio
+          Dinesh
         </Link>
         <div className='hidden lg:block'>{navList}</div>
 
